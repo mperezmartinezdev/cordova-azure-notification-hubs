0dc6d2787dd635662cae06e95a5c74c72859e668
Hendrik Kühnel <hendrik.kuehnel@melantech.de>
2021-06-22 12:08:47 +0200

    added "addtags" and "removeTag" to ios libary

--- src/ios/PushNotification.swift
+++ src/ios/PushNotification.swift
@@ -28,7 +28,7 @@ import WindowsAzureMessaging
     let installationId =  MSNotificationHub.getInstallationId()
 
 
-    if(installationId.isEmpty || deviceToken.isEmpty) {
+    if(MSNotificationHub) {
         let pushSelf:PushNotification = self
         NSLog("Waiting for push device registration")
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
@@ -51,12 +51,75 @@ import WindowsAzureMessaging
         let result: [String:String] = ["registrationId":deviceToken,"azureRegId":"$InstallationId:{"+installationId+"}"]
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: result);
         self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
-
     }
 
 
+    @objc(addTags:) // Declare your function name.
+  func addTags(command: CDVInvokedUrlCommand) { // write the function code.
+    let options = command.arguments[0] as! [String:Any]
+    let connectionString = options["connectionString"] as! String
+    let hubName = options["notificationHubPath"] as! String
+    NSLog("Tags", options["tags"])
+    if (options["tags"] != nil) {
+        let tagString = options["tags"] as? String
+        let tags = Set<AnyHashable>(tagString?.components(separatedBy: ","))
+    }
 
+    MSNotificationHub.setDelegate(self)
+    MSNotificationHub.start(connectionString: connectionString, hubName: hubName)
+    let deviceToken = MSNotificationHub.getPushChannel()
+    let installationId =  MSNotificationHub.getInstallationId()
 
+    if(installationId.isEmpty || deviceToken.isEmpty) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "Error - register device for push first");
+            NSLog("Error registering push device")
+            pushSelf.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+            pushSelf.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+        }
+    }
+    else {
+        if (tags != " " && tags != nil) {
+            NSLog("Add Tags to MSNotificationHub");
+            MSNotificationHub.addTags(tags);
+        } 
+        let result: [String:String] = ["registrationId":deviceToken,"azureRegId":"$InstallationId:{"+installationId+"}"]
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: result);
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+    }
+
+        @objc(removeTag:) // Declare your function name.
+  func removeTag(command: CDVInvokedUrlCommand) { // write the function code.
+    let options = command.arguments[0] as! [String:Any]
+    let connectionString = options["connectionString"] as! String
+    let hubName = options["notificationHubPath"] as! String
+    NSLog("Tag", options["tag"])
+    if (options["tag"] != nil) {
+        let tag = options["tag"] as? String
+    }
+
+    MSNotificationHub.setDelegate(self)
+    MSNotificationHub.start(connectionString: connectionString, hubName: hubName)
+    let deviceToken = MSNotificationHub.getPushChannel()
+    let installationId =  MSNotificationHub.getInstallationId()
+
+    if(installationId.isEmpty || deviceToken.isEmpty) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "Error - register device for push first");
+            NSLog("Error registering push device")
+            pushSelf.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+            pushSelf.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+        }
+    }
+    else {
+        if (tag != " " && tag != nil) {
+            NSLog("Add Tags to MSNotificationHub");
+            MSNotificationHub.removeTag(tag);
+        }  
+        let result: [String:String] = ["registrationId":deviceToken,"azureRegId":"$InstallationId:{"+installationId+"}"]
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: result);
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+    }
 
     /*
      * Always assume that the plugin will fail.
--- www/push.js
+++ www/push.js
@@ -14,150 +14,290 @@ var exec = cordova.require('cordova/exec');
  * @return {PushNotification} instance that can be monitored and cancelled.
  */
 
-var PushNotification = function(options) {
-    this._handlers = {
-        'registration': [],
-        'notification': [],
-        'error': []
-    };
+var PushNotification = function (options) {
+  this._handlers = {
+    registration: [],
+    notification: [],
+    error: [],
+  };
 
-    // require options parameter
-    if (typeof options === 'undefined') {
-        throw new Error('The options argument is required.');
-    }
+  // require options parameter
+  if (typeof options === 'undefined') {
+    throw new Error('The options argument is required.');
+  }
 
-    // store the options to this object instance
-    this.options = options;
+  // store the options to this object instance
+  this.options = options;
 
-    // triggered on registration and notification
-    var that = this;
-    var success = function(result) {
-        if (result && typeof result.registrationId !== 'undefined') {
-            that.emit('registration', result);
-        } else if (result && result.additionalData && typeof result.additionalData.actionCallback !== 'undefined') {
-            var executeFunctionOrEmitEventByName = function(callbackName, context, arg) {
-              var namespaces = callbackName.split('.');
-              var func = namespaces.pop();
-              for (var i = 0; i < namespaces.length; i++) {
-                context = context[namespaces[i]];
-              }
-
-              if (typeof context[func] === 'function') {
-                context[func].call(context, arg);
-              } else {
-                that.emit(callbackName, arg);
-              }
-            };
-
-            executeFunctionOrEmitEventByName(result.additionalData.actionCallback, window, result);
-        } else if (result) {
-            that.emit('notification', result);
+  // triggered on registration and notification
+  var that = this;
+  var success = function (result) {
+    if (result && typeof result.registrationId !== 'undefined') {
+      that.emit('registration', result);
+    } else if (
+      result &&
+      result.additionalData &&
+      typeof result.additionalData.actionCallback !== 'undefined'
+    ) {
+      var executeFunctionOrEmitEventByName = function (
+        callbackName,
+        context,
+        arg
+      ) {
+        var namespaces = callbackName.split('.');
+        var func = namespaces.pop();
+        for (var i = 0; i < namespaces.length; i++) {
+          context = context[namespaces[i]];
         }
-    };
 
-    // triggered on error
-    var fail = function(msg) {
-        var e = (typeof msg === 'string') ? new Error(msg) : msg;
-        that.emit('error', e);
-    };
+        if (typeof context[func] === 'function') {
+          context[func].call(context, arg);
+        } else {
+          that.emit(callbackName, arg);
+        }
+      };
 
-    // wait at least one process tick to allow event subscriptions
-    setTimeout(function() {
-        exec(success, fail, 'PushNotification', 'run', [options]);
-    }, 10);
+      executeFunctionOrEmitEventByName(
+        result.additionalData.actionCallback,
+        window,
+        result
+      );
+    } else if (result) {
+      that.emit('notification', result);
+    }
+  };
+
+  // triggered on error
+  var fail = function (msg) {
+    var e = typeof msg === 'string' ? new Error(msg) : msg;
+    that.emit('error', e);
+  };
+
+  // wait at least one process tick to allow event subscriptions
+  setTimeout(function () {
+    exec(success, fail, 'PushNotification', 'run', [options]);
+  }, 10);
 };
 
 /**
  * Unregister from push notifications
  */
 
-PushNotification.prototype.unregister = function(successCallback, errorCallback, options) {
-    if (!errorCallback) { errorCallback = function() {}; }
+PushNotification.prototype.unregister = function (
+  successCallback,
+  errorCallback,
+  options
+) {
+  if (!errorCallback) {
+    errorCallback = function () {};
+  }
 
-    if (typeof errorCallback !== 'function')  {
-        console.log('PushNotification.unregister failure: failure parameter not a function');
-        return;
+  if (typeof errorCallback !== 'function') {
+    console.log(
+      'PushNotification.unregister failure: failure parameter not a function'
+    );
+    return;
+  }
+
+  if (typeof successCallback !== 'function') {
+    console.log(
+      'PushNotification.unregister failure: success callback parameter must be a function'
+    );
+    return;
+  }
+
+  var that = this;
+  var cleanHandlersAndPassThrough = function () {
+    if (!options) {
+      that._handlers = {
+        registration: [],
+        notification: [],
+        error: [],
+      };
     }
+    successCallback();
+  };
 
-    if (typeof successCallback !== 'function') {
-        console.log('PushNotification.unregister failure: success callback parameter must be a function');
-        return;
-    }
+  exec(
+    cleanHandlersAndPassThrough,
+    errorCallback,
+    'PushNotification',
+    'unregister',
+    [options]
+  );
+};
 
-    var that = this;
-    var cleanHandlersAndPassThrough = function() {
-        if (!options) {
-            that._handlers = {
-                'registration': [],
-                'notification': [],
-                'error': []
-            };
-        }
-        successCallback();
-    };
+PushNotification.prototype.addTags = function (
+  successCallback,
+  errorCallback,
+  options
+) {
+  if (!errorCallback) {
+    errorCallback = function () {};
+  }
 
-    exec(cleanHandlersAndPassThrough, errorCallback, 'PushNotification', 'unregister', [options]);
+  if (typeof errorCallback !== 'function') {
+    console.log(
+      'PushNotification.addTags failure: failure parameter not a function'
+    );
+    return;
+  }
+
+  if (typeof successCallback !== 'function') {
+    console.log(
+      'PushNotification.addTags failure: success callback parameter must be a function'
+    );
+    return;
+  }
+
+  exec(
+    cleanHandlersAndPassThrough,
+    errorCallback,
+    'PushNotification',
+    'addTags',
+    [options]
+  );
+};
+
+PushNotification.prototype.removeTags = function (
+  successCallback,
+  errorCallback,
+  options
+) {
+  if (!errorCallback) {
+    errorCallback = function () {};
+  }
+
+  if (typeof errorCallback !== 'function') {
+    console.log(
+      'PushNotification.removeTags failure: failure parameter not a function'
+    );
+    return;
+  }
+
+  if (typeof successCallback !== 'function') {
+    console.log(
+      'PushNotification.removeTags failure: success callback parameter must be a function'
+    );
+    return;
+  }
+
+  exec(
+    cleanHandlersAndPassThrough,
+    errorCallback,
+    'PushNotification',
+    'removeTags',
+    [options]
+  );
 };
 
 /**
  * Call this to set the application icon badge
  */
 
-PushNotification.prototype.setApplicationIconBadgeNumber = function(successCallback, errorCallback, badge) {
-    if (!errorCallback) { errorCallback = function() {}; }
+PushNotification.prototype.setApplicationIconBadgeNumber = function (
+  successCallback,
+  errorCallback,
+  badge
+) {
+  if (!errorCallback) {
+    errorCallback = function () {};
+  }
 
-    if (typeof errorCallback !== 'function')  {
-        console.log('PushNotification.setApplicationIconBadgeNumber failure: failure parameter not a function');
-        return;
-    }
+  if (typeof errorCallback !== 'function') {
+    console.log(
+      'PushNotification.setApplicationIconBadgeNumber failure: failure parameter not a function'
+    );
+    return;
+  }
 
-    if (typeof successCallback !== 'function') {
-        console.log('PushNotification.setApplicationIconBadgeNumber failure: success callback parameter must be a function');
-        return;
-    }
+  if (typeof successCallback !== 'function') {
+    console.log(
+      'PushNotification.setApplicationIconBadgeNumber failure: success callback parameter must be a function'
+    );
+    return;
+  }
 
-    exec(successCallback, errorCallback, 'PushNotification', 'setApplicationIconBadgeNumber', [{badge: badge}]);
+  exec(
+    successCallback,
+    errorCallback,
+    'PushNotification',
+    'setApplicationIconBadgeNumber',
+    [{ badge: badge }]
+  );
 };
 
 /**
  * Get the application icon badge
  */
 
-PushNotification.prototype.getApplicationIconBadgeNumber = function(successCallback, errorCallback) {
-    if (!errorCallback) { errorCallback = function() {}; }
+PushNotification.prototype.getApplicationIconBadgeNumber = function (
+  successCallback,
+  errorCallback
+) {
+  if (!errorCallback) {
+    errorCallback = function () {};
+  }
 
-    if (typeof errorCallback !== 'function')  {
-        console.log('PushNotification.getApplicationIconBadgeNumber failure: failure parameter not a function');
-        return;
-    }
+  if (typeof errorCallback !== 'function') {
+    console.log(
+      'PushNotification.getApplicationIconBadgeNumber failure: failure parameter not a function'
+    );
+    return;
+  }
 
-    if (typeof successCallback !== 'function') {
-        console.log('PushNotification.getApplicationIconBadgeNumber failure: success callback parameter must be a function');
-        return;
-    }
+  if (typeof successCallback !== 'function') {
+    console.log(
+      'PushNotification.getApplicationIconBadgeNumber failure: success callback parameter must be a function'
+    );
+    return;
+  }
 
-    exec(successCallback, errorCallback, 'PushNotification', 'getApplicationIconBadgeNumber', []);
+  exec(
+    successCallback,
+    errorCallback,
+    'PushNotification',
+    'getApplicationIconBadgeNumber',
+    []
+  );
 };
 
 /**
  * Get the application icon badge
  */
 
-PushNotification.prototype.clearAllNotifications = function(successCallback, errorCallback) {
-    if (!successCallback) { successCallback = function() {}; }
-    if (!errorCallback) { errorCallback = function() {}; }
+PushNotification.prototype.clearAllNotifications = function (
+  successCallback,
+  errorCallback
+) {
+  if (!successCallback) {
+    successCallback = function () {};
+  }
+  if (!errorCallback) {
+    errorCallback = function () {};
+  }
 
-    if (typeof errorCallback !== 'function')  {
-        console.log('PushNotification.clearAllNotifications failure: failure parameter not a function');
-        return;
-    }
+  if (typeof errorCallback !== 'function') {
+    console.log(
+      'PushNotification.clearAllNotifications failure: failure parameter not a function'
+    );
+    return;
+  }
 
-    if (typeof successCallback !== 'function') {
-        console.log('PushNotification.clearAllNotifications failure: success callback parameter must be a function');
-        return;
-    }
+  if (typeof successCallback !== 'function') {
+    console.log(
+      'PushNotification.clearAllNotifications failure: success callback parameter must be a function'
+    );
+    return;
+  }
 
-    exec(successCallback, errorCallback, 'PushNotification', 'clearAllNotifications', []);
+  exec(
+    successCallback,
+    errorCallback,
+    'PushNotification',
+    'clearAllNotifications',
+    []
+  );
 };
 
 /**
@@ -173,11 +313,11 @@ PushNotification.prototype.clearAllNotifications = function(successCallback, err
  * @param {Function} callback triggered on the event.
  */
 
-PushNotification.prototype.on = function(eventName, callback) {
-    if (!this._handlers.hasOwnProperty(eventName)) {
-        this._handlers[eventName] = [];
-    }
-    this._handlers[eventName].push(callback);
+PushNotification.prototype.on = function (eventName, callback) {
+  if (!this._handlers.hasOwnProperty(eventName)) {
+    this._handlers[eventName] = [];
+  }
+  this._handlers[eventName].push(callback);
 };
 
 /**
@@ -188,12 +328,12 @@ PushNotification.prototype.on = function(eventName, callback) {
  */
 
 PushNotification.prototype.off = function (eventName, handle) {
-    if (this._handlers.hasOwnProperty(eventName)) {
-        var handleIndex = this._handlers[eventName].indexOf(handle);
-        if (handleIndex >= 0) {
-            this._handlers[eventName].splice(handleIndex, 1);
-        }
+  if (this._handlers.hasOwnProperty(eventName)) {
+    var handleIndex = this._handlers[eventName].indexOf(handle);
+    if (handleIndex >= 0) {
+      this._handlers[eventName].splice(handleIndex, 1);
     }
+  }
 };
 
 /**
@@ -207,42 +347,54 @@ PushNotification.prototype.off = function (eventName, handle) {
  * @return {Boolean} is true when the event is triggered otherwise false.
  */
 
-PushNotification.prototype.emit = function() {
-    var args = Array.prototype.slice.call(arguments);
-    var eventName = args.shift();
+PushNotification.prototype.emit = function () {
+  var args = Array.prototype.slice.call(arguments);
+  var eventName = args.shift();
 
-    if (!this._handlers.hasOwnProperty(eventName)) {
-        return false;
+  if (!this._handlers.hasOwnProperty(eventName)) {
+    return false;
+  }
+
+  for (var i = 0, length = this._handlers[eventName].length; i < length; i++) {
+    var callback = this._handlers[eventName][i];
+    if (typeof callback === 'function') {
+      callback.apply(undefined, args);
+    } else {
+      console.log('event handler: ' + eventName + ' must be a function');
     }
+  }
 
-    for (var i = 0, length = this._handlers[eventName].length; i < length; i++) {
-        var callback = this._handlers[eventName][i];
-        if (typeof callback === 'function') {
-            callback.apply(undefined,args);
-        } else {
-            console.log('event handler: ' + eventName + ' must be a function');
-        }
-    }
-
-    return true;
+  return true;
 };
 
-PushNotification.prototype.finish = function(successCallback, errorCallback, id) {
-    if (!successCallback) { successCallback = function() {}; }
-    if (!errorCallback) { errorCallback = function() {}; }
-    if (!id) { id = 'handler'; }
+PushNotification.prototype.finish = function (
+  successCallback,
+  errorCallback,
+  id
+) {
+  if (!successCallback) {
+    successCallback = function () {};
+  }
+  if (!errorCallback) {
+    errorCallback = function () {};
+  }
+  if (!id) {
+    id = 'handler';
+  }
 
-    if (typeof successCallback !== 'function') {
-        console.log('finish failure: success callback parameter must be a function');
-        return;
-    }
+  if (typeof successCallback !== 'function') {
+    console.log(
+      'finish failure: success callback parameter must be a function'
+    );
+    return;
+  }
 
-    if (typeof errorCallback !== 'function')  {
-        console.log('finish failure: failure parameter not a function');
-        return;
-    }
+  if (typeof errorCallback !== 'function') {
+    console.log('finish failure: failure parameter not a function');
+    return;
+  }
 
-    exec(successCallback, errorCallback, 'PushNotification', 'finish', [id]);
+  exec(successCallback, errorCallback, 'PushNotification', 'finish', [id]);
 };
 
 /*!
@@ -250,31 +402,37 @@ PushNotification.prototype.finish = function(successCallback, errorCallback, id)
  */
 
 module.exports = {
-    /**
-     * Register for Push Notifications.
-     *
-     * This method will instantiate a new copy of the PushNotification object
-     * and start the registration process.
-     *
-     * @param {Object} options
-     * @return {PushNotification} instance
-     */
+  /**
+   * Register for Push Notifications.
+   *
+   * This method will instantiate a new copy of the PushNotification object
+   * and start the registration process.
+   *
+   * @param {Object} options
+   * @return {PushNotification} instance
+   */
 
-    init: function(options) {
-        return new PushNotification(options);
-    },
+  init: function (options) {
+    return new PushNotification(options);
+  },
 
-    hasPermission: function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'PushNotification', 'hasPermission', []);
-    },
+  hasPermission: function (successCallback, errorCallback) {
+    exec(
+      successCallback,
+      errorCallback,
+      'PushNotification',
+      'hasPermission',
+      []
+    );
+  },
 
-    /**
-     * PushNotification Object.
-     *
-     * Expose the PushNotification object for direct use
-     * and testing. Typically, you should use the
-     * .init helper method.
-     */
+  /**
+   * PushNotification Object.
+   *
+   * Expose the PushNotification object for direct use
+   * and testing. Typically, you should use the
+   * .init helper method.
+   */
 
-    PushNotification: PushNotification
+  PushNotification: PushNotification,
 };
